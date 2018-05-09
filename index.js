012596e6020d842ce23cc7910a8719ac5eccda98
Emmanuel Owusu <e_owusu@live.com>
2018-05-09 06:36:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ const randomNumber = Math.floor(Math.random()*20) + 1;
 
 const randomInteger = Math.floor(randomNumber);
 
-const randomUserID = randomInteger + 1
+const randomUserID = randomInteger + 11000000002
